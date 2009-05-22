e6766b3ed907d5f862591fe0a57b5b3067c6cb09
Giuseppe Coviello <cjg@cruxppc.org>
2009-05-22 16:09:20 +0200

    added GradsLibrary

---
+++ GradsLibrary/GradsLibrary/CommandOutput.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+
+namespace GradsLibrary
+{
+    public class CommandOutput
+    {
+        private string output;
+        private int result_code;
+
+        public CommandOutput(StreamReader sr)
+        {
+            bool ipc_found = false;
+            string line;
+            output = "";
+            result_code = -1;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (ipc_found && line.StartsWith("<RC>"))
+                {
+                    string s = line.Substring(line.IndexOf(' '));
+                    s = s.Substring(0, s.IndexOf('<'));
+                    result_code = int.Parse(s.Trim());
+                    break;
+                }
+                if (ipc_found)
+                {
+                    output += line + "\n";
+                }
+                if (line.StartsWith("<IPC>"))
+                    ipc_found = true;
+            }
+        }
+
+        public int ResultCode
+        {
+            get
+            {
+                return result_code;
+            }
+        }
+
+        public string Output
+        {
+            get
+            {
+                return output;
+            }
+        }
+
+        public string[] OutputArray
+        {
+            get
+            {
+                return output.Split('\n');
+            }
+        }
+    }
+}
